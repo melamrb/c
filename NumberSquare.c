9041e8295f720627970028688467c00c492cd92c
Rajendra B M <melamrb@gmail.com>
2025-12-24 16:17:46 +0530

    Implement square calculation for user input
    Added functionality to calculate the square of a given number.

--- NumberSquare.c
+++ NumberSquare.c
@@ -1,12 +1,22 @@
 /*****************
 Name: WAP to find square of a given number
-
-
 ******************/
 #include<stdio.h>
+#include<conio.h>
 
 int main(void) 
 {
-    puts("Hello World!");
+    int Number_u16 = 0;
+    int NumSquare_u16 = 0; // Datatype couldn't contain square of int for bigger numbers. To be improved
+    
+    printf("Enter any number to find out the sqaure of it : \n ");
+    scanf("%d", &Number_u16);
+    
+    NumSquare_u16 = (Number_u16*Number_u16);
+    
+    printf("\nSquare of entered number is :%d",NumSquare_u16);
+    
+    getch();
+    
     return 0;
 }
